7b5be77ef2bfab871b1982376cb13c59e771d5ff
Sam Manzi <smanzi@Sams-MacBook-Pro.local>
2020-08-09 10:06:15 -0400

    removing redundancies from data_loading.py

--- src/data_loading.py
+++ src/data_loading.py
@@ -92,24 +92,3 @@ class NewsText(Dataset):
     def __getitem__(self, idx):
         sample = self.news_text_list[idx], self.labels[idx]
         return sample
-
-def pad_sent(sents, max_seq_len):
-    max_seq_len = min(100, max_seq_len)
-    out = []
-    lens = []
-    for i in range(len(sents)):
-        sent = sents[i]
-        lens.append(len(sent))
-        append_tensor = tensor([sent[j] if j < len(sent) else 0 for j in range(max_seq_len)]).unsqueeze(0)
-        out.append(append_tensor)
-    out = torch.cat(out)
-    return out, lens
-
-
-def collate_fn(sample):
-
-    labels = tensor([s[1] for s in sample])
-    sents = [s[0] for s in sample]
-    max_seq_len = max([sent.shape[0] for sent in sents])
-    sents, lens = pad_sent(sents, max_seq_len)
-    return sents, labels, lens
